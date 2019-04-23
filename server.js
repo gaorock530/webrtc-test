525aaaf1b3eb660969ca055a6f2ee5411ece8606
gaorock530 <gaorock530@hotmail.com>
2019-04-24 02:22:55 +0800

    add ssl

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 const express = require('express');
+const https = require('https');
+const fs = require('fs');
+const path = require('path');
+
+const options = {
+  key: fs.readFileSync(path.join(__dirname, 'ssl', 'key.key')),
+  cert: fs.readFileSync(path.join(__dirname, 'ssl', 'cert.crt'))
+};
+
+
 
 const app = express();
 
 app.use(express.static(__dirname + '/build'));
-app.listen(3000, (e) => console.log(e || 'server running on Port: 3000'));
+
+const httpsServer = https.createServer(options, app);
+httpsServer.listen(3000, (e) => console.log(e || 'server running on Port: 3000'));
